8c2fc35b45b790a66d77eeae228b2029791f38ce
tomcatmi6 <31080750+tomcatmi6@users.noreply.github.com>
2017-09-07 11:11:01 +0200

    score app v 0.1

---
+++ score.js
@@ -0,0 +1,53 @@
+var num = document.getElementById("number");
+var scr = document.getElementById("score");
+
+num.addEventListener("click", function() { 
+    score.innerHTML = num.value;
+});
+
+//zrobic zeby po kliknieciu dodawalo graczowi punkt
+var pl1 = document.querySelectorAll("button")[0];
+var pl2 = document.querySelectorAll("button")[1];
+
+var pl1goal = document.getElementById("play1");
+var pl2goal = document.getElementById("play2");
+var i = 1;
+var j = 1;
+pl1.addEventListener("click", function() { 
+    if(i<num.value){
+        pl1goal.innerHTML =i++
+       }
+      else if (i=num.value) {
+          pl1goal.innerHTML =i++
+          pl1goal.style.color ="blue";
+      }
+
+      else {
+        pl1goal.style.color ="blue";
+      }
+})
+
+pl2.addEventListener("click", function() { 
+    if(j<num.value){
+        pl2goal.innerHTML =j++
+       }
+      else if (j=num.value) {
+          pl2goal.innerHTML =j++
+          pl2goal.style.color ="blue";
+      }
+
+      else {
+        pl2goal.style.color ="blue";
+      }
+})
+
+// reset
+var res = document.querySelectorAll("button")[2];
+res.addEventListener("click", function() {
+    var i = 1;
+    var j = 1;
+    pl1goal.innerHTML = 0; 
+    pl2goal.innerHTML = 0;
+
+})
+
