910a909925d1fe25b9aa1156688ad4baf1fcb3fe
tomcatmi6 <31080750+tomcatmi6@users.noreply.github.com>
2017-09-19 22:54:38 +0200

    wersja klockow po zmianach. Do poprawki kolor przy resecie zeby sie restowal

--- rgb.js
+++ rgb.js
@@ -1,17 +1,17 @@
 var sqr = document.getElementsByClassName("kwadrat");
 var rgbmain = document.getElementById("rgbhead");
+var newColor = document.getElementById("newColor");
 
 run();
-run3();
+
 
 losuj();
 losuj1();
-losujdla3();
 color();
 
 sqrToColor();
 newNumber();
-newNumberfor3();
+
 
 function losuj() {
   return Math.floor(Math.random() * 255);
@@ -19,11 +19,6 @@ function losuj() {
 
 function losuj1() {
   for (var i = 0; i < sqr.length; i++) {
-    sqr[i].style.backgroundColor = color();
-  }
-}
-function losujdla3() {
-  for (var i = 0; i < 3; i++) {
     sqr[i].style.backgroundColor = color();
   }
 }
@@ -37,10 +32,6 @@ function newNumber() {
   rgbmain.innerHTML = sqr[goodOne].style.backgroundColor;
 }
 
-function newNumberfor3() {
-  var goodOne = Math.floor(Math.random() * 3);
-  rgbmain.innerHTML = sqr[goodOne].style.backgroundColor;
-}
 function sqrToColor() {
   for (var i = 0; i < sqr.length; i++) {
     sqr[i].style.backgroundColor = rgbmain.innerHTML;
@@ -48,6 +39,15 @@ function sqrToColor() {
     sqr[i].style.opacity = "1";
     sqr[i].style.transition = "opacity 1s ease-in";
   }
+}
+
+function reset() {
+  for (var i = 0; i < sqr.length; i++) {
+    sqr[i].style.opacity = "1";
+    sqr[i].style.transition = "opacity 1s ease-in";
+  }
+  tryAgain.innerHTML = "";
+  run();
 }
 
 function run() {
@@ -75,55 +75,9 @@ function run() {
   sqrWrap.addEventListener("click", isCorrect);
 }
 
-// tu jest fcja dla 3 kafelkow:
-function run3() {
-  losuj();
-  color();
-  losuj1();
-
-  var rgbmain = document.getElementById("rgbhead");
-
-  newNumber();
-
-  var tryAgain = document.getElementById("tryAgain");
-  var sqrWrap = document.getElementById("row");
-  var head = document.getElementById("head");
-  function isCorrect() {
-    if (event.target.style.backgroundColor == rgbmain.innerHTML) {
-      tryAgain.innerHTML = "Great, you made it!";
-      head.style.backgroundColor = rgbmain.innerHTML;
-
-      sqrToColor();
-
-      console.log("this.style.backgroundColor");
-    } else {
-      console.log("baaba");
-      tryAgain.innerHTML = "Try Again!";
-      event.target.style.opacity = "0";
-      event.target.style.transition = "opacity 1s ease-out";
-    }
-  }
-
-  sqrWrap.addEventListener("click", isCorrect);
-}
-// run();
-function reset() {
-  for (var i = 0; i < sqr.length; i++) {
-    sqr[i].style.opacity = "1";
-    sqr[i].style.transition = "opacity 1s ease-in";
-  }
-  tryAgain.innerHTML = "";
-  run();
-}
 run();
-var newColor = document.getElementById("newColor");
 
-// newColor.onclick = losuj;
-// newColor.onclick = color;
-// newColor.onclick = losuj1;
+
 newColor.onclick = reset;
 
-var easy = document.getElementById("easy");
-var hard = document.getElementById("hard");
-easy.onclick = run3;
-hard.onclick = run;
+
